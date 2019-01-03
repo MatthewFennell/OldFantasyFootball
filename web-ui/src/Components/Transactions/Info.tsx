ccce8cdbdcac2843293e2194dab953db6e5fa046
MattFennell <MFennell@scottlogic.com>
2019-01-03 12:22:47 +0000

    No useless constructor

--- web-ui/src/Components/Transactions/Info.tsx
+++ web-ui/src/Components/Transactions/Info.tsx
@@ -4,10 +4,6 @@ import '../../Style/Info.css';
 interface InfoProps {}
 
 class Info extends React.Component<InfoProps> {
-  constructor(props: InfoProps) {
-    super(props);
-  }
-
   render() {
     return <div className="transaction-info-page" />;
   }
