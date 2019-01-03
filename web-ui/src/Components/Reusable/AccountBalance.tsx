b192f8911acadac1a9d816a863fe8234f454eddd
MattFennell <MFennell@scottlogic.com>
2019-01-03 12:20:35 +0000

    More removal of transaction stuff

--- web-ui/src/Components/Reusable/AccountBalance.tsx
+++ web-ui/src/Components/Reusable/AccountBalance.tsx
@@ -1,57 +1,13 @@
 import * as React from 'react';
 import '../../Style/AccountBalance.css';
-import { getUser } from '../../Services/UserService';
-import DisplayMoney from './DisplayMoney';
 
-interface Props {
-  setBalance: (value: number) => void;
-  setFirstName: (value: string) => void;
-  balance: number;
-  firstName: string;
-}
+interface Props {}
 
 class AccountBalance extends React.Component<Props, {}> {
-  shouldComponentUpdate(nextProps: Props, nextState: {}) {
-    return nextProps !== this.props || nextState !== this.state;
-  }
-
-  componentDidMount() {
-    getUser()
-      .then(response => {
-        this.props.setBalance(parseFloat(response.balance));
-        this.props.setFirstName(response.firstName);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  componentDidMount() {}
 
   public render() {
-    const { firstName, balance } = this.props;
-    return (
-      <div id="container-account-balance">
-        <div id="balance" className="unselectable shadow">
-          {!firstName || !balance ? (
-            <div className="loading-spinner-container-balance">
-              <img className="loading-spinner-balance" />
-            </div>
-          ) : (
-            <div>
-              <span id="AccountBalance-Debit" className="text-center">
-                {firstName}
-                's debit
-                <br />
-                account
-              </span>
-              <span id="AccountBalance-Value" className="text-center">
-                <DisplayMoney balance={balance} transactionRow={false} />
-              </span>
-              <span id="AccountBalance-Text">Account Balance</span>
-            </div>
-          )}
-        </div>
-      </div>
-    );
+    return <div id="container-account-balance" />;
   }
 }
 export default AccountBalance;
