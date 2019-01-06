e1ce60d6dd7d4aba22cd46af149dd44216de05e0
Matt <mfennell@scottlogic.com>
2019-01-06 20:29:54 +0000

    A pitch exists

---
+++ web-ui/src/Components/Team/PitchLayout/Pitch.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import '../../../Style/Team/PitchLayout/Pitch.css';
+
+class Pitch extends React.Component<{}, {}> {
+  render() {
+    return <div className="pitch-background" />;
+  }
+}
+export default Pitch;
--- web-ui/src/Components/Team/Team.tsx
+++ web-ui/src/Components/Team/Team.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import '../../Style/Team/Team.css';
 import Info from '../../Containers/Team/Info';
 import Stats from '../../Containers/Team/Stats';
+import Pitch from '../../Containers/Team/PitchLayout/Pitch';
 
 interface TransactionsProps {}
 
@@ -27,7 +28,9 @@ class Transactions extends React.Component<TransactionsProps, TransactionsState>
         <div className="row-2-stats">
           <Stats />
         </div>
-        <div className="row-3-squad">Squad</div>
+        <div className="row-3-squad">
+          <Pitch />
+        </div>
         <div className="row-4-bench">Bench</div>
       </div>
     );
