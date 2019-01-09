06ec30524f0a2d0f71a346aa3a5c068efb855ecc
MattFennell <MFennell@scottlogic.com>
2019-01-09 15:40:32 +0000

    server-side league stuff completed (COULD BE IMPROVED)

---
+++ web-services/src/main/java/uk/co/scottlogic/gradProject/server/repos/LeagueManager.java
@@ -0,0 +1,103 @@
+package uk.co.scottlogic.gradProject.server.repos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.co.scottlogic.gradProject.server.repos.documents.ApplicationUser;
+import uk.co.scottlogic.gradProject.server.repos.documents.League;
+import uk.co.scottlogic.gradProject.server.repos.documents.UsersWeeklyTeam;
+import uk.co.scottlogic.gradProject.server.routers.dto.LeagueReturnDTO;
+import uk.co.scottlogic.gradProject.server.routers.dto.TopWeeklyUserReturnDTO;
+import uk.co.scottlogic.gradProject.server.routers.dto.UserPatchDTO;
+import uk.co.scottlogic.gradProject.server.routers.dto.WeeklyPlayerReturnDTO;
+
+import java.util.*;
+
+@Service
+public class LeagueManager {
+
+    private ApplicationUserRepo applicationUserRepo;
+
+    private WeeklyTeamRepo weeklyTeamRepo;
+
+    private WeeklyTeamManager weeklyTeamManager;
+
+    private LeagueRepo leagueRepo;
+
+    @Autowired
+    public LeagueManager(ApplicationUserRepo applicationUserRepo, WeeklyTeamRepo weeklyTeamRepo,
+                         WeeklyTeamManager weeklyTeamManager, LeagueRepo leagueRepo) {
+        this.applicationUserRepo = applicationUserRepo;
+        this.weeklyTeamRepo = weeklyTeamRepo;
+        this.weeklyTeamManager = weeklyTeamManager;
+        this.leagueRepo = leagueRepo;
+
+        Optional<ApplicationUser> user = applicationUserRepo.findByUsername("a");
+        if (user.isPresent()){
+            List<LeagueReturnDTO> response = findLeaguesPlayerIsIn(user.get());
+            for (LeagueReturnDTO dto : response){
+                System.out.println("League name = " + dto.getLeagueName() + ", position = " + dto.getPosition());
+            }
+        }
+    }
+
+    public void createLeague(ApplicationUser owner, String leagueName, Integer startWeek, String codeToJoin){
+        League league = new League(owner, leagueName, new ArrayList<>(), startWeek, codeToJoin);
+        league.addParticipant(owner);
+        leagueRepo.save(league);
+    }
+
+    // Sorts them by total points (doesn't look at points since week started!!!!)
+    // Top points is first
+    public List<ApplicationUser> findUsersInLeague(League league){
+        List<ApplicationUser> participants = league.getParticipants();
+        participants.sort(Comparator.comparing(ApplicationUser::getTotalPoints).reversed());
+        return participants;
+    }
+
+    // Need to stop the same player joining a league multiple times
+    // Need to make the code for joining obscure
+    public void addPlayerToLeague(ApplicationUser user, String code){
+
+        Optional<League> league = leagueRepo.findByCodeToJoin(code);
+        if (league.isPresent()) {
+            League l = league.get();
+            String leagueCode = l.getCodeToJoin();
+            if (leagueCode.equals(code)) {
+                l.addParticipant(user);
+                leagueRepo.save(l);
+            }
+            else {
+                throw new IllegalArgumentException("Invalid code for league");  // Never happens?
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Invalid code for league");
+        }
+    }
+
+    // Change this to binary search
+    // SURELY this can be improved
+    public List<LeagueReturnDTO> findLeaguesPlayerIsIn(ApplicationUser user){
+
+        List<LeagueReturnDTO> returnDTOS = new ArrayList<>();
+        Iterable<League> userLeagues = leagueRepo.findAll();
+
+        List<League> allLeagues = new ArrayList<>();
+        userLeagues.forEach(allLeagues::add);
+
+        for (League l : allLeagues){
+            List<ApplicationUser> participants = findUsersInLeague(l);
+            Integer position = 0;
+            for (ApplicationUser u : participants){
+                position += 1;
+                if (u.getId().equals(user.getId())){
+                    returnDTOS.add(new LeagueReturnDTO(l.getLeagueName(), position));
+                    break;
+                }
+            }
+        }
+        return returnDTOS;
+
+    }
+
+}
---
+++ web-services/src/main/java/uk/co/scottlogic/gradProject/server/routers/LeagueController.java
@@ -0,0 +1,101 @@
+package uk.co.scottlogic.gradProject.server.routers;
+
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import uk.co.scottlogic.gradProject.server.misc.ExceptionLogger;
+import uk.co.scottlogic.gradProject.server.misc.Icons;
+import uk.co.scottlogic.gradProject.server.repos.*;
+import uk.co.scottlogic.gradProject.server.repos.documents.ApplicationUser;
+import uk.co.scottlogic.gradProject.server.repos.documents.PlayerPoints;
+import uk.co.scottlogic.gradProject.server.routers.dto.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Api(value = "Authentication", description = "Operations pertaining to Players")
+public class LeagueController {
+
+    private static final Logger log = LoggerFactory.getLogger(Token.class);
+
+    private LeagueManager leagueManager;
+
+    @Autowired
+    public LeagueController(LeagueManager leagueManager) {
+        this.leagueManager = leagueManager;
+    }
+
+    @ApiOperation(value = Icons.key + " Make a league ", authorizations = {
+            @Authorization(value = "jwtAuth")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Never returned but swagger won't let me get rid of it"),
+            @ApiResponse(code = 201, message = "Category successfully added"),
+            @ApiResponse(code = 400, message = "Must provide valid category name"),
+            @ApiResponse(code = 403, message = "Insufficient privileges"),
+            @ApiResponse(code = 500, message = "Server Error")})
+    @PostMapping(value = "/league/make")
+    public void makeLeague(@AuthenticationPrincipal ApplicationUser user,
+                                    @RequestBody MakeLeagueDTO dto, HttpServletResponse response) {
+        try {
+                leagueManager.createLeague(user, dto.getLeagueName(), dto.getStartWeek(), dto.getCodeToJoin());
+        } catch (DuplicateKeyException e) {
+            response.setStatus(409);
+        } catch (IllegalArgumentException e) {
+            response.setStatus(400);
+        }
+    }
+
+    @ApiOperation(value = Icons.key + " Join a league ", authorizations = {
+            @Authorization(value = "jwtAuth")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Never returned but swagger won't let me get rid of it"),
+            @ApiResponse(code = 201, message = "Category successfully added"),
+            @ApiResponse(code = 400, message = "Must provide valid category name"),
+            @ApiResponse(code = 403, message = "Insufficient privileges"),
+            @ApiResponse(code = 500, message = "Server Error")})
+    @PostMapping(value = "/league/join")
+    public void addPlayerToLeague(@AuthenticationPrincipal ApplicationUser user,
+                           @RequestBody String code, HttpServletResponse response) {
+        try {
+            leagueManager.addPlayerToLeague(user, code);
+        } catch (DuplicateKeyException e) {
+            response.setStatus(409);
+        } catch (IllegalArgumentException e) {
+            response.setStatus(400);
+        }
+        catch (Exception e){
+            response.setStatus(400);
+        }
+    }
+
+    @ApiOperation(value = Icons.key
+            + " Find all leagues that the user is in",
+            notes = "Requires User role", authorizations = {
+            @Authorization(value = "jwtAuth")})
+    @GetMapping("/league/user/all")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Returned successfully"),
+            @ApiResponse(code = 400, message = "Invalid date / category"),
+            @ApiResponse(code = 500, message = "Server Error")})
+    @PreAuthorize("hasRole('USER')")
+    public List<LeagueReturnDTO> getLeaguesByUser(
+            @AuthenticationPrincipal ApplicationUser user, HttpServletResponse response){
+        try {
+            // Currently just returns the randomly first selected
+            // Should go back later and make it choose the top on some criteria
+            return leagueManager.findLeaguesPlayerIsIn(user);
+        } catch (Exception e) {
+            response.setStatus(403);
+        }
+        return Collections.emptyList();
+    }
+
+}
