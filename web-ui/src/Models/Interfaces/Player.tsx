dfe90321925efa8d25b3a303f5505b90c1f53b64
Matt <mfennell@scottlogic.com>
2019-01-15 19:33:59 +0000

    dafuq is going on

---
+++ web-ui/src/Models/Interfaces/Player.tsx
@@ -0,0 +1,15 @@
+export interface Player {
+  id: string;
+  firstName: string;
+  surname: string;
+  position: string;
+  points: string;
+  price: string;
+  weeklyGoals: string;
+  weeklyAssists: string;
+  weeklyPoints: string;
+  totalScore: string;
+  totalGoals: string;
+  totalAssists: string;
+  collegeTeam: string;
+}
