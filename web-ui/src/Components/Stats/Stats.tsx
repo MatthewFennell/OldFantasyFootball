93f1a7107bf2a78bea7331d7547bdd15ecf60a26
Matt <mfennell@scottlogic.com>
2019-04-13 23:45:21 +0100

    Nice ish UI for for history page

--- web-ui/src/Components/Stats/Stats.tsx
+++ web-ui/src/Components/Stats/Stats.tsx
@@ -4,13 +4,9 @@ import { SingleHistory } from '../../Models/Interfaces/SingleHistory';
 import { TeamHistory } from '../../Models/Interfaces/TeamHistory';
 import CustomDropdown from '../common/CustomDropdown';
 import '../../Style/Stats/Stats.css';
-import CollegeTeam from '../../Containers/Admin/AddPointsCollegeTeam';
-import { CollegeTeam as CT } from '../../Models/Interfaces/CollegeTeam';
 import TeamStats from './TeamStats';
 
 interface SettingsProps {
-	  allCollegeTeams: CT[];
-
     setStatsHistory: (week:number, statsHistory: SingleHistory) => void;
     statsHistory: TeamHistory[][];
     totalNumberOfWeeks: number;
@@ -18,18 +14,14 @@ interface SettingsProps {
 
 interface SettingsState {
 		week: number;
-		collegeName: string;
   }
 
 class Settings extends React.Component<SettingsProps, SettingsState> {
 	constructor (props: SettingsProps) {
 		super(props);
 		this._handleWeek = this._handleWeek.bind(this);
-		this._handleCollegeName = this._handleCollegeName.bind(this);
-		const { allCollegeTeams } = this.props;
 		this.state = {
 			week: this.props.totalNumberOfWeeks,
-			collegeName: allCollegeTeams.length > 0 ? allCollegeTeams[0].name : ''
 		};
 		this.getHistory(this.state.week);
 	}
@@ -37,8 +29,6 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
 		if (!this.props.statsHistory[week]) {
 			getStats(week).then(response => {
 				this.props.setStatsHistory(week, response);
-				this.props.setStatsHistory(1, response);
-				this.props.setStatsHistory(2, response);
 			})
 				.catch(error => {
 					console.log('error = ' + error);
@@ -51,17 +41,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
 		this.getHistory(week);
 	}
 
-	_handleCollegeName (collegeName: string) {
-		this.setState({ collegeName });
-	}
-
 	render () {
-		let currentHistory = this.props.statsHistory[this.state.week];
-		if (currentHistory) {
-			let p = currentHistory.find(a => a.teamName === this.state.collegeName);
-			console.log('p = ' + JSON.stringify(p));
-		}
-
 		return (
 			<div className="stats-wrapper">
 
@@ -73,24 +53,51 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
 							values={[...Array(this.props.totalNumberOfWeeks + 1).keys()]}
 						/>
 					</div>
-
-					<div className="stats-team-selector">
-						<CollegeTeam setTeam={this._handleCollegeName} />
-					</div>
 				</div>
 
 				<div className="all-team-stats-wrapper">
 
-					{this.props.statsHistory[this.state.week]
+					<div className="stats-history-columns">
+						{this.props.statsHistory[this.state.week]
 
-						? this.props.statsHistory[this.state.week].map(team => (
-							<TeamStats
-								assists={team.assists}
-								goalScorers={team.goalScorers}
-								key="a"
-								teamName={team.teamName}
-							/>))
-						: null}
+							? this.props.statsHistory[this.state.week].map((team, index) => (
+								index < this.props.statsHistory[this.state.week].length / 3
+									? <TeamStats
+										assists={team.assists}
+										goalScorers={team.goalScorers}
+										key="a"
+										teamName={team.teamName}
+									  /> : null))
+							: null}
+					</div>
+
+					<div className="stats-history-columns">
+						{this.props.statsHistory[this.state.week]
+
+							? this.props.statsHistory[this.state.week].map((team, index) => (
+								index >= this.props.statsHistory[this.state.week].length / 3 && index < this.props.statsHistory[this.state.week].length * 2 / 3
+									? <TeamStats
+										assists={team.assists}
+										goalScorers={team.goalScorers}
+										key="a"
+										teamName={team.teamName}
+									  /> : null))
+							: null}
+					</div>
+
+					<div className="stats-history-columns">
+						{this.props.statsHistory[this.state.week]
+
+							? this.props.statsHistory[this.state.week].map((team, index) => (
+								index >= this.props.statsHistory[this.state.week].length * 2 / 3
+									? <TeamStats
+										assists={team.assists}
+										goalScorers={team.goalScorers}
+										key="a"
+										teamName={team.teamName}
+									  /> : null))
+							: null}
+					</div>
 				</div>
 			</div>
 		);
--- web-ui/src/Containers/Stats/Stats.tsx
+++ web-ui/src/Containers/Stats/Stats.tsx
@@ -3,12 +3,10 @@ import { State } from '../../Reducers/root';
 import { setStatsHistory } from '../../Actions/StatsActions';
 import { getStatsHistory, getTotalNumberOfWeeks } from '../../Selectors/StatsSelector';
 import Stats from '../../Components/Stats/Stats';
-import { getAllCollegeTeams } from '../../Selectors/AdminSelector';
 
 const mapStateToProps = (state: State) => ({
 	statsHistory: getStatsHistory(state),
 	totalNumberOfWeeks: getTotalNumberOfWeeks(state),
-	allCollegeTeams: getAllCollegeTeams(state)
 });
 
 const mapDispatchToProps = {
