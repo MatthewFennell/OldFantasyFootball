633b3f67915acd289f737855168ea4bf21116658
Matt <mfennell@scottlogic.com>
2019-05-19 13:16:41 +0100

    Fixed test

--- web-services/src/main/java/uk/co/scottlogic/gradProject/server/repos/documents/ApplicationUser.java
+++ web-services/src/main/java/uk/co/scottlogic/gradProject/server/repos/documents/ApplicationUser.java
@@ -249,12 +249,11 @@ public class ApplicationUser implements UserDetails, Serializable {
     }
 
     public void setTeamName(String teamName) {
-        System.out.println("setting team name to " + teamName);
         if (!teamName.matches(USER_TEAM_NAME_PATTERN)) {
             throw new IllegalArgumentException("Team name does not match regex");
         }
         for (String PROFANITY : PROFANITIES) {
-            if (username.toLowerCase().contains(PROFANITY)) {
+            if (teamName.toLowerCase().contains(PROFANITY)) {
                 throw new IllegalArgumentException("No profanities plz");
             }
         }
